2804286c7c56b14099a57803233f40ae296e8fd3
baekyungtae <>
2023-04-26 10:44:59 +0900

    프록시 팩토리 테스트

--- src/test/java/hello/proxy/common/service/ServiceImpl.java
+++ src/test/java/hello/proxy/common/service/ServiceImpl.java
@@ -1,4 +1,4 @@
-package hello.proxy.pureproxy.proxy.common.service;
+package hello.proxy.common.service;
 
 import lombok.extern.slf4j.Slf4j;
 
--- src/test/java/hello/proxy/common/service/ServiceInterface.java
+++ src/test/java/hello/proxy/common/service/ServiceInterface.java
@@ -1,4 +1,4 @@
-package hello.proxy.pureproxy.proxy.common.service;
+package hello.proxy.common.service;
 
 public interface ServiceInterface {
 
